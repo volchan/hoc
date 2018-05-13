8585f67cc92fe3d7088eefec572c2aedbd411feb
stephane de la torre <s.dlt@hotmail.fr>
2018-05-13 15:30:57 +0200

    session 4/39 done

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,8 @@
 import { CHANGE_AUTH } from "./types";
 
-const authenticate = isLoggedIn => {
+export const authenticate = isLoggedIn => {
   return {
     type: CHANGE_AUTH,
     payload: isLoggedIn
   };
 };
-
-export default authenticate;
--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,19 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router';
+import React, { Component } from "react";
+import { Link } from "react-router";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 class Header extends Component {
   authButton() {
-    return <button>Sign In</button>;
+    if (this.props.authenticated) {
+      return (
+        <button onClick={() => this.props.authenticate(false)}>Sign Out</button>
+      );
+    } else {
+      return (
+        <button onClick={() => this.props.authenticate(true)}>Sign In</button>
+      );
+    }
   }
 
   render() {
@@ -16,12 +26,15 @@ class Header extends Component {
           <li className="nav-item">
             <Link to="/resources">Resources</Link>
           </li>
-          <li className="nav-item">
-            {this.authButton()}
-          </li>
+          <li className="nav-item">{this.authButton()}</li>
         </ul>
       </nav>
     );
   }
 }
-export default Header;
+
+const mapStateToProps = state => {
+  return { authenticated: state.authenticated };
+};
+
+export default connect(mapStateToProps, actions)(Header);
