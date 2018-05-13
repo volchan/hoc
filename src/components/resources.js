979fef8378f50913fc4aea614db7f8208ed2e480
stephane de la torre <s.dlt@hotmail.fr>
2018-05-13 15:07:07 +0200

    session 4/37 done

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      Super SpecialSecret Recipe
+      <ul>
+        <li>1 Cup Sugar</li>
+        <li>1 Cup Pepper</li>
+        <li>1 Cup Salt</li>
+      </ul>
+    </div>
+  );
+};
